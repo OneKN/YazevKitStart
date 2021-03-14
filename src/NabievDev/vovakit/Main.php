4cb216d45abd9371b326f5d567a12e065a872053
OneKN <38722958+OneKN@users.noreply.github.com>
2021-03-14 15:51:26 +0500

    Язевизация плагина

--- src/NabievDev/vovakit/Main.php
+++ src/NabievDev/vovakit/Main.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace NabievDev\vovakit;
+namespace NabievDev\Yazev;
 
-use NabievDev\kitstart\commands\VovaKitStart;
+use NabievDev\kitstart\commands\YazevKitStart;
 use pocketmine\item\Item;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
@@ -10,7 +10,7 @@ use pocketmine\Server;
 
 /**
  * Class Main
- * @package NabievDev\vovakit
+ * @package NabievDev\yazev
  */
 class Main extends PluginBase
 {
@@ -19,8 +19,8 @@ class Main extends PluginBase
      */
     public function onEnable()
     {
-        Server::getInstance()->getCommandMap()->register("vovakitstart", new VovaKitStart("vovakitstart", "VovaKit one love <3"));
-        $this->getLogger()->info("§b[VovaKitStart] §7Данный плагин был создан как шутка для Вовы Кота §bhttps://vk.com/vovan446");
+        Server::getInstance()->getCommandMap()->register("yazevkitstart", new YazevKitStart("yazevkitstart", "Yazev my crash <3"));
+        $this->getLogger()->info("§b[YazevKitStart] §7Данный плагин является форком шуточного плагина от Набиева");
     }
 
     /**
@@ -28,11 +28,11 @@ class Main extends PluginBase
      */
     public static function giveKit(Player $player)
     {
-        $player->getArmorInventory()->setHelmet(Item::get(Item::IRON_HELMET, 0, 1)->setCustomName("§bШлем Вовки"));
-        $player->getArmorInventory()->setChestplate(Item::get(Item::IRON_CHESTPLATE, 0, 1)->setCustomName("§bГрудак Вовки"));
-        $player->getArmorInventory()->setLeggings(Item::get(Item::IRON_LEGGINGS, 0, 1)->setCustomName("§bШтанишки Вовки"));
-        $player->getArmorInventory()->setBoots(Item::get(Item::IRON_BOOTS, 0, 1)->setCustomName("§bСандали Вовки"));
-        $player->getInventory()->setItem(0, Item::get(Item::STONE_SWORD, 0, 1)->setCustomName("§bПалочка-ебалочка Вовки"));
+        $player->getArmorInventory()->setHelmet(Item::get(Item::IRON_HELMET, 0, 1)->setCustomName("§bШлем"));
+        $player->getArmorInventory()->setChestplate(Item::get(Item::IRON_CHESTPLATE, 0, 1)->setCustomName("§bГрудак"));
+        $player->getArmorInventory()->setLeggings(Item::get(Item::IRON_LEGGINGS, 0, 1)->setCustomName("§bШтанишки"));
+        $player->getArmorInventory()->setBoots(Item::get(Item::IRON_BOOTS, 0, 1)->setCustomName("§bСандали"));
+        $player->getInventory()->setItem(0, Item::get(Item::STONE_SWORD, 0, 1)->setCustomName("§bПалочка-ебалочка Пети"));
         //Кому надо еще что-то или что-то переделать, то делайте сами, мне впадлу)
     }
 }
